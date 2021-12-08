8e73dbf7f133a8dc69fc112e2246d2fca437cc12
Serhii <sergeybutr@gmail.com>
2021-12-08 12:00:32 +0200

    Fixed mistackes in comments

--- src/main/kotlin/lesson2_classes_sample/Person.kt
+++ src/main/kotlin/lesson2_classes_sample/Person.kt
@@ -7,15 +7,22 @@ package lesson2_classes_sample
 /**
  *  Example of a simple Data class.
  *
- *  Compiler generated functions:
+ *  Compiler generates functions:
  *  1. equals() / hashCode() pair
  *  2. toString()
  *  3. componentN() corresponding to the properties in their order of declaration
  *  4. copy()
+ *
+ *  Restrictions:
+ *
+ *  1. Data classes can inherit abstract classes, other classes and implement interface, but
+ *  it can't inherit other Data classes (open keyword is NOT allowed on Data classes)
+ *
+ *  2. varargs are not allowed (due to some JVM limitations)
  *
  */
 
 /**
  * Compiler automatically generates fields and their setters and getters if it's needed
  */
-class Person(val name: String, var isMarried: Boolean)
+data class Person(val name: String, var isMarried: Boolean)
